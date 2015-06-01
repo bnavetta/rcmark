6805c4a229da42de28affa291e71db7a5bad908c
Ben Navetta <ben.navetta@gmail.com>
2015-05-31 22:25:25 -0400

    add node accessors

--- src/node.rs
+++ src/node.rs
@@ -1,5 +1,9 @@
 use {raw, NodeType, ListType, DelimType, NodeIterator};
 
+use std::ffi::{CStr, CString};
+use std::str;
+use libc;
+
 // The representation of nodes is made somewhat difficult by the fact that
 // libcmark uses cmark_node* for both owned and unowned references. For example,
 // both cmark_node_new and cmark_node_parent return a cmark_node*, but only
@@ -92,6 +96,156 @@ impl Node {
     pub fn iter(&self) -> NodeIterator {
         NodeIterator::new(self)
     }
+
+    pub fn node_type(&self) -> NodeType {
+        NodeType::from_raw(unsafe {
+            raw::cmark_node_get_type(self.raw)
+        })
+    }
+
+    pub fn node_type_string(&self) -> &str {
+        unsafe {
+            let c_str =
+                CStr::from_ptr(raw::cmark_node_get_type_string(self.raw));
+            str::from_utf8(c_str.to_bytes()).unwrap()
+        }
+    }
+
+    pub fn literal(&self) -> Option<&str> {
+        unsafe {
+            from_string_ptr(raw::cmark_node_get_literal(self.raw))
+        }
+    }
+
+    pub fn set_literal(&mut self, content: &str) -> bool {
+        unsafe {
+            let cstr = CString::new(content).unwrap();
+            to_bool(raw::cmark_node_set_literal(self.raw, cstr.as_ptr()))
+        }
+    }
+
+    pub fn header_level(&self) -> i32 {
+        unsafe {
+            raw::cmark_node_get_header_level(self.raw) as i32
+        }
+    }
+
+    pub fn set_header_level(&mut self, level: i32) -> bool {
+        unsafe {
+            to_bool(raw::cmark_node_set_header_level(self.raw, level as libc::c_int))
+        }
+    }
+
+    pub fn list_type(&self) -> ListType {
+        unsafe {
+           ListType::from_raw(raw::cmark_node_get_list_type(self.raw))
+        }
+    }
+
+    pub fn set_list_type(&mut self, list_type: ListType) -> bool {
+        unsafe {
+            to_bool(raw::cmark_node_set_list_type(self.raw, list_type.raw()))
+        }
+    }
+
+    pub fn delim_type(&self) -> DelimType {
+        unsafe {
+            DelimType::from_raw(raw::cmark_node_get_delim_type(self.raw))
+        }
+    }
+
+    pub fn set_delim_type(&mut self, delim_type: DelimType) -> bool {
+        unsafe {
+            to_bool(raw::cmark_node_set_delim_type(self.raw, delim_type.raw()))
+        }
+    }
+
+    pub fn list_start(&self) -> i32 {
+        unsafe {
+            raw::cmark_node_get_list_start(self.raw) as i32
+        }
+    }
+
+    pub fn set_list_start(&mut self, start: i32) -> bool {
+        unsafe {
+            to_bool(raw::cmark_node_set_list_start(self.raw, start as libc::c_int))
+        }
+    }
+
+    pub fn list_tight(&self) -> bool {
+        unsafe {
+            to_bool(raw::cmark_node_get_list_tight(self.raw))
+        }
+    }
+
+    pub fn set_list_tight(&mut self, tight: bool) -> bool {
+        unsafe {
+            to_bool(raw::cmark_node_set_list_tight(self.raw, from_bool(tight)))
+        }
+    }
+
+    pub fn fence_info(&self) -> Option<&str> {
+        unsafe {
+            from_string_ptr(raw::cmark_node_get_fence_info(self.raw))
+        }
+    }
+
+    pub fn set_fence_info(&mut self, info: &str) -> bool {
+        unsafe {
+            let cstr = CString::new(info).unwrap();
+            to_bool(raw::cmark_node_set_fence_info(self.raw, cstr.as_ptr()))
+        }
+    }
+
+    pub fn url(&self) -> Option<&str> {
+        unsafe {
+            from_string_ptr(raw::cmark_node_get_url(self.raw))
+        }
+    }
+
+    pub fn set_url(&mut self, url: &str) -> bool {
+        unsafe {
+            let cstr = CString::new(url).unwrap();
+            to_bool(raw::cmark_node_set_url(self.raw, cstr.as_ptr()))
+        }
+    }
+
+    pub fn title(&self) -> Option<&str> {
+        unsafe {
+            from_string_ptr(raw::cmark_node_get_title(self.raw))
+        }
+    }
+
+    pub fn set_title(&mut self, title: &str) -> bool {
+        unsafe {
+            let cstr = CString::new(title).unwrap();
+            to_bool(raw::cmark_node_set_title(self.raw, cstr.as_ptr()))
+        }
+    }
+
+    pub fn start_line(&self) -> i32 {
+        unsafe {
+            raw::cmark_node_get_start_line(self.raw) as i32
+        }
+    }
+
+    pub fn start_column(&self) -> i32 {
+        unsafe {
+            raw::cmark_node_get_start_column(self.raw) as i32
+        }
+    }
+
+    pub fn end_column(&self) -> i32 {
+        unsafe {
+            raw::cmark_node_get_end_column(self.raw) as i32
+        }
+    }
+
+    pub fn end_line(&self) -> i32 {
+        unsafe {
+            raw::cmark_node_get_end_line(self.raw) as i32
+        }
+    }
 }
 
 impl Drop for Node {
@@ -99,5 +253,43 @@ impl Drop for Node {
         if self.owned {
             unsafe { raw::cmark_node_free(self.raw) }
         }
+    }
+}
+
+fn to_bool(val: libc::c_int) -> bool {
+    match val {
+        0 => false,
+        1 => true,
+        _ => panic!("Cannot convert to bool: {}", val)
+    }
+}
+
+fn from_bool(val: bool) -> libc::c_int {
+    match val {
+        true => 1 as libc::c_int,
+        false => 0 as libc::c_int,
+    }
+}
+
+unsafe fn from_string_ptr<'a>(ptr: *const libc::c_char) -> Option<&'a str> {
+    if ptr.is_null() {
+        None
+    }
+    else {
+        let c_str = CStr::from_ptr(ptr);
+        str::from_utf8(c_str.to_bytes()).ok()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use super::super::NodeType;
+
+    #[test]
+    fn test_node_type() {
+        let node = Node::new(NodeType::List);
+        assert!(node.node_type() == NodeType::List);
+        assert!(node.node_type_string() == "list");
     }
 }
